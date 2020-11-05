ab3c7b51e3dc4a755ea851b1893382d65218dfdf
rutvikrj <rutvikrj26@gmail.com>
2020-11-05 23:55:39 +0530

    First trial

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -3,47 +3,37 @@
     <!-- Name -->
     <div class="field">
       <label class="label is-medium">Name</label>
-      <div class="control has-icons-left">
         <input class="input is-medium" type="text" placeholder="John Smith" />
-      </div>
     </div>
     <!-- Email -->
     <div class="field">
       <label class="label is-medium">Email</label>
-      <div class="control has-icons-left">
         <input
           class="input is-medium"
           type="email"
           placeholder="you@email.com"
         />
-      </div>
     </div>
     <!-- Subject -->
     <div class="field">
       <label class="label is-medium">Subject</label>
-      <div class="control has-icons-left">
         <input
           class="input is-medium"
           type="text"
           placeholder="Brief Summary"
         />
-      </div>
     </div>
     <!-- Message -->
     <div class="field">
       <label class="label is-medium">Message</label>
-      <div class="control">
         <textarea
           class="textarea"
           placeholder="Detailed description of your comment, request, etc"
           rows="8"
         ></textarea>
-      </div>
     </div>
-    <div class="control">
       <button class="button is-primary">
         Contact Us
       </button>
-    </div>
   </form>
 </template>
